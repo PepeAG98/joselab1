c5b593ce74bfda1a964f14f1b5c1b797992d0cb6
PepeAG98 <A01273109@itesm.mx>
2019-03-08 09:49:37 -0600

    3 de 4

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
@@ -11,12 +12,33 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
+
+
 class DogeMeme extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			input1: '',
+			input2: ''
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleChange(event) {
+		const target = event.target;
+		const name = target.name;
+			this.setState({[name]: event.target.value.toUpperCase()});
+		}
+
 	render(){
 		return(
 			<div className='dog'>
-				<h1>EASY PEASY</h1>
-				<h1>KEMON SQUEEZY</h1>
+			<input name="input1" type="text" value={this.state.input1} onChange={this.handleChange} />
+			<input name="input2" type="text" value={this.state.input2} onChange={this.handleChange} />
+				<div className='caja'>
+					<h1 className='centrar1'>{this.state.input1}</h1>
+					<h1 className='centrar2'>{this.state.input2}</h1>
+				</div>
 			</div>
 		)
 	}
